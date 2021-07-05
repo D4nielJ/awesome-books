de0c6b511ca6118839e2429ae4403cada2db3429
D4nielJ <d4niel.djm@gmail.com>
2021-07-05 16:22:34 -0500

    Add functions to update DOM and add book

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@
 
 const formAddBook = document.querySelector("form");
 const submitButton = document.querySelector("form > button");
+const booksIndex = document.querySelector(".books-index");
 
 // Object constructor
 
@@ -10,7 +11,26 @@ function CreateBook(title, author) {
   this.author = author;
 }
 
-const booksArray = [];
+// Functions to manage books from the collection
+
+const booksArray = [
+  {
+    title: "Harry Potter",
+    author: "JK Rowling",
+  },
+  {
+    title: "Lord of the Rings",
+    author: "Tolkien",
+  },
+  {
+    title: "Test",
+    author: "Jhon Doe",
+  },
+  {
+    title: "Another Test",
+    author: "Jhon Doe",
+  },
+];
 
 function addBook(e) {
   e.preventDefault();
@@ -18,8 +38,30 @@ function addBook(e) {
   newBook.title = formAddBook.title.value;
   newBook.author = formAddBook.author.value;
   booksArray.push(newBook);
+  addOneBookToDom(1);
+}
+
+function removeBook() {}
+
+// Function insert books in the DOM
+
+function addOneBookToDom(i) {
+  const li = document.createElement("li");
+  li.innerHTML = `<h2>${booksArray[booksArray.length - i].title}</h2>
+    <h3>${booksArray[booksArray.length - i].author}</h3>
+    <button type="button">Remove</button>`;
+  li.classList.add("books-index__item");
+  booksIndex.appendChild(li);
+}
+
+function appendCollectionToDom() {
+  booksIndex.innerHTML = "";
+  for (let i = booksArray.length; i > 0; i -= 1) {
+    addOneBookToDom(i);
+  }
 }
 
 // Events
 
+window.onload = appendCollectionToDom;
 formAddBook.addEventListener("submit", addBook);
