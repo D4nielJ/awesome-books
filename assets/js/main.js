0efd986becaf00ba8e4eb83a1927462fbd1158dc
D4nielJ <d4niel.djm@gmail.com>
2021-07-06 07:00:45 -0500

    Fix bug with DOM

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 // Selectors
 
 let booksArray = [];
-const formAddBook = document.querySelector("form");
-const getBooksIndex = () => document.querySelector(".books-index");
+const formAddBook = document.querySelector('form');
+const getBooksIndex = () => document.querySelector('.books-index');
 
 // Object constructor
 
@@ -15,21 +15,20 @@ function CreateBook(title, author) {
 
 function removeBook(bookTitle) {
   console.log(booksArray);
-  const book = booksArray.filter(book => book.title === bookTitle);
+  const book = booksArray.filter((book) => book.title === bookTitle);
   const index = booksArray.indexOf(book[0]);
   booksArray.splice(index, 1);
   updateStorage();
   const booksIndex = getBooksIndex();
+  console.log(index);
+  console.log(booksIndex.childNodes);
   const bookDom = booksIndex.childNodes[index];
+  console.log(bookDom);
   bookDom.remove();
 }
 
-const addEventListenerToRemoveBtn = (button) => {
-  button.addEventListener('click', (e) => removeBook(e.target.parentNode.firstChild.textContent));
-}
-
 function addOneBookToDom(i) {
-  const li = document.createElement("li");
+  const li = document.createElement('li');
   li.innerHTML = `<h2>${booksArray[booksArray.length - i].title}</h2>
     <h3>${booksArray[booksArray.length - i].author}</h3>`;
   const removeButton = document.createElement('button');
@@ -37,7 +36,7 @@ function addOneBookToDom(i) {
   removeButton.setAttribute('type', 'button');
   addEventListenerToRemoveBtn(removeButton);
   li.appendChild(removeButton);
-  li.classList.add("books-index__item");
+  li.classList.add('books-index__item');
   const booksIndex = getBooksIndex();
   booksIndex.appendChild(li);
 }
@@ -65,36 +64,35 @@ const storageAvailable = (type) => {
     storage.removeItem(x);
     return true;
   } catch (e) {
-    return e instanceof DOMException && (
+    return (
+      e instanceof DOMException &&
       // everything except Firefox
-      e.code === 22
-      // Firefox
-      || e.code === 1014
-      // test name field too, because code might not be present
-      // everything except Firefox
-      || e.name === 'QuotaExceededError'
-      // Firefox
-      || e.name === 'NS_ERROR_DOM_QUOTA_REACHED')
-    // acknowledge QuotaExceededError only if there's something already stored
-    && (storage && storage.length !== 0);
+      (e.code === 22 ||
+        // Firefox
+        e.code === 1014 ||
+        // test name field too, because code might not be present
+        // everything except Firefox
+        e.name === 'QuotaExceededError' ||
+        // Firefox
+        e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+      // acknowledge QuotaExceededError only if there's something already stored
+      storage &&
+      storage.length !== 0
+    );
   }
 };
 
 const updateStorage = () => {
   localStorage.setItem('storedBooksIndex', JSON.stringify(booksArray));
-}
+};
 
 const loadStorage = (storedBooksIndex) => {
   storedBooksIndex = JSON.parse(storedBooksIndex);
   storedBooksIndex.forEach((book) => {
-    // console.log()
-    const newBook = new CreateBook();
-    newBook.title = book.title;
-    newBook.author = book.author;
     booksArray.push(newBook);
     addOneBookToDom(1);
   });
-}
+};
 
 const load = () => {
   if (storageAvailable('localStorage')) {
@@ -107,4 +105,9 @@ const load = () => {
 // Events
 
 window.onload = load;
-formAddBook.addEventListener("submit", addBook);
+formAddBook.addEventListener('submit', addBook);
+const addEventListenerToRemoveBtn = (button) => {
+  button.addEventListener('click', (e) =>
+    removeBook(e.target.parentNode.firstChild.textContent)
+  );
+};
