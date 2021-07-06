717f3732f8be0c4d7c864ff88b0f30cd4c458d47
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-07-06 13:44:52 -0600

    fix linter errors and refactor regular functions into arrow functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+/* eslint-disable max-classes-per-file */
+
 // Class constructor
 class Book {
   constructor(title, author) {
@@ -26,39 +28,37 @@ class BooksCollection {
 
 // Selectors
 
-const formAddBook = document.querySelector("form");
-const booksIndex = document.querySelector(".books-index");
+const formAddBook = document.querySelector('form');
+const booksIndex = document.querySelector('.books-index');
 const bookCollection = new BooksCollection();
 
 const updateStorage = () => {
-  localStorage.setItem("storedBooksIndex", JSON.stringify(bookCollection.booksArray));
+  localStorage.setItem('storedBooksIndex', JSON.stringify(bookCollection.booksArray));
 };
 
-function removeBook(bookTitle) {
+const removeBook = (bookTitle) => {
   const index = bookCollection.removeBookFromCollection(bookTitle);
   updateStorage();
   const bookDom = booksIndex.children[index];
   bookDom.remove();
-}
-
-const addEventListenerToRemoveBtn = (button) => {
-  button.addEventListener("click", (e) =>
-    removeBook(e.target.parentNode.firstChild.textContent)
-  );
 };
 
-function addOneBookToDom(i) {
-  const li = document.createElement("li");
+const addEventListenerToRemoveBtn = (button) => {
+  button.addEventListener('click', (e) => removeBook(e.target.parentNode.firstChild.textContent));
+};
+
+const addOneBookToDom = (i) => {
+  const li = document.createElement('li');
   li.innerHTML = `<h2>${bookCollection.booksArray[bookCollection.booksArray.length - i].title}</h2>
     <h3>${bookCollection.booksArray[bookCollection.booksArray.length - i].author}</h3>`;
-  const removeButton = document.createElement("button");
-  removeButton.textContent = "Remove";
-  removeButton.setAttribute("type", "button");
+  const removeButton = document.createElement('button');
+  removeButton.textContent = 'Remove';
+  removeButton.setAttribute('type', 'button');
   addEventListenerToRemoveBtn(removeButton);
   li.appendChild(removeButton);
-  li.classList.add("books-index__item");
+  li.classList.add('books-index__item');
   booksIndex.appendChild(li);
-}
+};
 
 // Local Storage
 
@@ -66,25 +66,25 @@ const storageAvailable = (type) => {
   let storage;
   try {
     storage = window[type];
-    const x = "__storage_test__";
+    const x = '__storage_test__';
     storage.setItem(x, x);
     storage.removeItem(x);
     return true;
   } catch (e) {
     return (
-      e instanceof DOMException &&
+      e instanceof DOMException
       // everything except Firefox
-      (e.code === 22 ||
+      && (e.code === 22
         // Firefox
-        e.code === 1014 ||
+        || e.code === 1014
         // test name field too, because code might not be present
         // everything except Firefox
-        e.name === "QuotaExceededError" ||
+        || e.name === 'QuotaExceededError'
         // Firefox
-        e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
+        || e.name === 'NS_ERROR_DOM_QUOTA_REACHED')
       // acknowledge QuotaExceededError only if there's something already stored
-      storage &&
-      storage.length !== 0
+      && storage
+      && storage.length !== 0
     );
   }
 };
@@ -99,17 +99,17 @@ const loadStorage = (storedBooksIndex) => {
 
 // Functions to manage books from the collection
 
-function addBook(e) {
+const addBook = (e) => {
   e.preventDefault();
   bookCollection.addBookToCollection(formAddBook.title.value, formAddBook.author.value);
   updateStorage();
   addOneBookToDom(1);
-}
+};
 
 const load = () => {
-  if (storageAvailable("localStorage")) {
+  if (storageAvailable('localStorage')) {
     if (localStorage.length !== 0) {
-      loadStorage(localStorage.getItem("storedBooksIndex"));
+      loadStorage(localStorage.getItem('storedBooksIndex'));
     }
   }
 };
@@ -117,4 +117,4 @@ const load = () => {
 // Events
 
 window.onload = load;
-formAddBook.addEventListener("submit", addBook);
+formAddBook.addEventListener('submit', addBook);
