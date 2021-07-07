ba22796221accee4126bf7d0fe92b1cf68167920
D4nielJ <d4niel.djm@gmail.com>
2021-07-07 17:42:52 -0500

    Fix linter errors

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,27 @@
-import BooksCollection from "./booksCollectionModule.js";
+import BooksCollection from './booksCollectionModule.js';
 
 // Selectors
 
-const formAddBook = document.querySelector("form");
-const booksIndex = document.querySelector(".books-index");
+const formAddBook = document.querySelector('form');
+const booksIndex = document.querySelector('.books-index');
 const bookCollection = new BooksCollection();
+
+// Front-end
+
+const styleTable = () => {
+  if (bookCollection.booksArray.length === 0) {
+    booksIndex.classList.remove('books-index--border');
+  } else {
+    booksIndex.classList.add('books-index--border');
+  }
+};
 
 const updateStorage = () => {
   localStorage.setItem(
-    "storedBooksIndex",
-    JSON.stringify(bookCollection.booksArray)
+    'storedBooksIndex',
+    JSON.stringify(bookCollection.booksArray),
   );
-  localStorage.setItem("storedBookIdCounter", bookCollection.bookIdCounter);
+  localStorage.setItem('storedBookIdCounter', bookCollection.bookIdCounter);
 };
 
 const removeBook = (bookCard, id) => {
@@ -22,29 +32,26 @@ const removeBook = (bookCard, id) => {
 };
 
 const addEventListenerToRemoveBtn = (button) => {
-  button.addEventListener("click", (e) =>
-    removeBook(
-      e.target.parentNode,
-      parseInt(e.target.parentNode.dataset.bookid, 10)
-    )
-  );
+  button.addEventListener('click', (e) => removeBook(
+    e.target.parentNode,
+    parseInt(e.target.parentNode.dataset.bookid, 10),
+  ));
 };
 
 const addOneBookToDom = (i) => {
-  const li = document.createElement("li");
+  const li = document.createElement('li');
   // if (bookCollection.booksArray.length % 2 === 0) {
   //   li.classList.add("books-index__item--alt");
   // }
-  const newBook =
-    bookCollection.booksArray[bookCollection.booksArray.length - i];
+  const newBook = bookCollection.booksArray[bookCollection.booksArray.length - i];
   li.innerHTML = `<p class="books-index__title"><span>${newBook.title}</span> by ${newBook.author}</p>`;
-  const removeButton = document.createElement("button");
-  removeButton.textContent = "Remove";
-  removeButton.setAttribute("type", "button");
+  const removeButton = document.createElement('button');
+  removeButton.textContent = 'Remove';
+  removeButton.setAttribute('type', 'button');
   addEventListenerToRemoveBtn(removeButton);
   li.appendChild(removeButton);
-  li.classList.add("books-index__item");
-  li.setAttribute("data-bookid", `${newBook.bookid}`);
+  li.classList.add('books-index__item');
+  li.setAttribute('data-bookid', `${newBook.bookid}`);
   booksIndex.appendChild(li);
 };
 
@@ -54,25 +61,25 @@ const storageAvailable = (type) => {
   let storage;
   try {
     storage = window[type];
-    const x = "__storage_test__";
+    const x = '__storage_test__';
     storage.setItem(x, x);
     storage.removeItem(x);
     return true;
   } catch (e) {
     return (
-      e instanceof DOMException &&
+      e instanceof DOMException
       // everything except Firefox
-      (e.code === 22 ||
+      && (e.code === 22
         // Firefox
-        e.code === 1014 ||
+        || e.code === 1014
         // test name field too, because code might not be present
         // everything except Firefox
-        e.name === "QuotaExceededError" ||
+        || e.name === 'QuotaExceededError'
         // Firefox
-        e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
+        || e.name === 'NS_ERROR_DOM_QUOTA_REACHED')
       // acknowledge QuotaExceededError only if there's something already stored
-      storage &&
-      storage.length !== 0
+      && storage
+      && storage.length !== 0
     );
   }
 };
@@ -92,7 +99,7 @@ const addBook = (e) => {
   e.preventDefault();
   bookCollection.addBookToCollection(
     formAddBook.title.value,
-    formAddBook.author.value
+    formAddBook.author.value,
   );
   updateStorage();
   addOneBookToDom(1);
@@ -100,12 +107,12 @@ const addBook = (e) => {
 };
 
 const load = () => {
-  if (storageAvailable("localStorage")) {
+  if (storageAvailable('localStorage')) {
     if (localStorage.length !== 0) {
-      const storedBooksIndex = localStorage.getItem("storedBooksIndex");
+      const storedBooksIndex = localStorage.getItem('storedBooksIndex');
       const storedBookIdCounter = parseInt(
-        localStorage.getItem("storedBookIdCounter"),
-        10
+        localStorage.getItem('storedBookIdCounter'),
+        10,
       );
       loadStorage(storedBooksIndex, storedBookIdCounter);
     }
@@ -113,17 +120,7 @@ const load = () => {
   styleTable();
 };
 
-// Front-end
-
-const styleTable = () => {
-  if (bookCollection.booksArray.length === 0) {
-    booksIndex.classList.remove("books-index--border");
-  } else {
-    booksIndex.classList.add("books-index--border");
-  }
-};
-
 // Events
 
 window.onload = load;
-formAddBook.addEventListener("submit", addBook);
+formAddBook.addEventListener('submit', addBook);
